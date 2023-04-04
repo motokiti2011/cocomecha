7ca06994f229dc80e79573a5a7214c595a3dcd88
motokiti2011 <motokiti2011@gmail.com>
2023-04-04 18:47:19 +0900

    44 work

--- src/app/page/menu/footer-menu/inquiry/inquiry.component.ts
+++ src/app/page/menu/footer-menu/inquiry/inquiry.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-inquiry',
@@ -7,7 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InquiryComponent implements OnInit {
 
-  constructor() { }
+  /** フォームコントロール */
+  name = new FormControl('名前', [
+    Validators.required
+  ]);
+
+  mail = new FormControl('メール', [
+    Validators.required
+  ]);
+
+  /** フォームグループオブジェクト */
+  groupForm = this.builder.group({
+    name: this.name,
+    mail: this.mail,
+  })
+
+  constructor(
+    private builder: FormBuilder,
+    public modal: MatDialog,
+  ) { }
 
   ngOnInit(): void {
   }
